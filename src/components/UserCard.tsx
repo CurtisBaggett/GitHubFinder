0a5bb140663d04e4a7c1955f83f58472bd9b4c59
CurtisBaggett <51203203+CurtisBaggett@users.noreply.github.com>
2026-05-15 15:08:46 -0400

    Commented imports

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,44 +1,44 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
-import { toast } from "sonner";
-import {
-  checkIfFollowingUser,
-  followGithubUser,
-  unfollowGithubUser,
-} from "../api/github";
+// import { useQuery, useMutation } from "@tanstack/react-query";
+// import { toast } from "sonner";
+// import {
+//   checkIfFollowingUser,
+//   followGithubUser,
+//   unfollowGithubUser,
+// } from "../api/github";
 import { FaGithubAlt } from "react-icons/fa";
 import type { GitHubUser } from "../types";
 
 const UserCard = ({ user }: { user: GitHubUser }) => {
   //Query to check if user is following
-  const { data: isFollowing, refetch } = useQuery({
-    queryKey: ["follow-status", user.login],
-    queryFn: () => checkIfFollowingUser(user.login),
-    enabled: !!user.login,
-  });
+  // const { data: isFollowing, refetch } = useQuery({
+  //   queryKey: ["follow-status", user.login],
+  //   queryFn: () => checkIfFollowingUser(user.login),
+  //   enabled: !!user.login,
+  // });
 
   //Mutation to follow the user
-  const followMutation = useMutation({
-    mutationFn: () => followGithubUser(user.login),
-    onSuccess: () => {
-      toast.success(`You are now following ${user.login}`);
-      refetch();
-    },
-    onError: (err) => {
-      toast.error(err.message);
-    },
-  });
+  // const followMutation = useMutation({
+  //   mutationFn: () => followGithubUser(user.login),
+  //   onSuccess: () => {
+  //     toast.success(`You are now following ${user.login}`);
+  //     refetch();
+  //   },
+  //   onError: (err) => {
+  //     toast.error(err.message);
+  //   },
+  // });
 
   //Mutation to unfollow the user
-  const unfollowMutation = useMutation({
-    mutationFn: () => unfollowGithubUser(user.login),
-    onSuccess: () => {
-      toast.success(`You are no longer following ${user.login}`);
-      refetch();
-    },
-    onError: (err) => {
-      toast.error(err.message);
-    },
-  });
+  // const unfollowMutation = useMutation({
+  //   mutationFn: () => unfollowGithubUser(user.login),
+  //   onSuccess: () => {
+  //     toast.success(`You are no longer following ${user.login}`);
+  //     refetch();
+  //   },
+  //   onError: (err) => {
+  //     toast.error(err.message);
+  //   },
+  // });
 
   // const handleFollow = () => {
   //   if (isFollowing) {
